e689eac5ff668d1d8f661cdd22918eac837578c2
Suanming Mou <suanmingm@mellanox.com>
2020-08-18 22:26:23 +0800

    no interactive

--- testpmd-ui.py
+++ testpmd-ui.py
@@ -3,15 +3,20 @@
 from scapy.all import *
 from flexx import flx, app, ui, event
 
+
 class DeviceBox(flx.PyWidget):
 
 	def init(self):
 		self.box=[]
-		with flx.VBox():
-			flx.Label(text='-------- Device List --------', flex=1, css_class="center")
-			for i, dev in enumerate(self.root.interface_list):
-				txt=" PCI_ID: " + self.root.pci_list[i] + " mac: " + self.root.mac_list[i].upper() + "Interface: " + dev 
-				self.box.append(flx.CheckBox(text=txt))
+		with flx.HSplit(flex=1,style='border:2px solid gray;border-radius: 5px'):
+			with flx.VBox(flex=1):
+				flx.Label(text=' ', flex=1)
+				flx.Label(text='Device List', flex=1)
+				flx.Label(text=' ', flex=1)
+			with flx.VBox(flex=3):
+				for i, dev in enumerate(self.root.interface_list):
+					txt=" PCI_ID: " + self.root.pci_list[i] + " mac: " + self.root.mac_list[i].upper() + "Interface: " + dev 
+					self.box.append(flx.CheckBox(text=txt))
 
 	@flx.reaction
 	def a_button_was_pressed(self):
@@ -25,51 +30,84 @@ class EALBox(flx.PyWidget):
 
 	def init(self):
 		self.box=[]
-		with flx.VBox():
-			flx.Label(text='-------- EAL  args --------', flex=1, css_class="center")
+		with flx.VBox(flex=1, title = 'EAL args', style='border:2px solid gray;border-radius: 5px'):
 			with flx.HBox():
-				ui.Label(text='--master-core:', flex=3)
-				self.mc = ui.LineEdit(text='0', flex=2)
-				ui.Label(text='-c COREMASK:', flex=3)
-				self.cm = ui.LineEdit(text='0xffff', flex=2)
+				ui.Label(flex=1,text='EAL args:  ')
+				ui.Label(flex=0,text='--master-lcore:')
+				self.mc = ui.LineEdit(flex=2,text='0')
+				ui.Label(flex=1,text='')
+				ui.Label(flex=0, text="    -c COREMASK:")
+				self.cm = ui.LineEdit(flex=2,text='0xff')
+				ui.Label(flex=1,text='')
+				ui.Label(flex=0, text='    --file-prefix:')
+				self.fp = ui.LineEdit(flex=2,text='/tmp/')
+				self.nohp = flx.CheckBox(flex=2,text='--no-huge')
+				self.hpunlink = flx.CheckBox(flex=2,text='--huge-unlink')
 
 	def get_eal_arg(self):
-		eal_arg = "--master-core="+self.mc.text + " -c " + self.cm.text
+		eal_arg = "--master-lcore="+self.mc.text + " -c " + self.cm.text
 		return eal_arg
 
 class APPBox(flx.PyWidget):
 
 	def init(self):
 		self.box=[]
-		with flx.VBox():
-			flx.Label(text='-------- Testpmd args --------', flex=1, css_class="center")
-			with flx.HBox():
-				self.cb_as = flx.CheckBox(text="Auto-start")
+		with flx.VBox(flex=1,  style='border:2px solid gray;border-radius: 5px;text-align:left'):
+			with flx.HFix(flex=1, style='text-align:left'):
+				flx.Label(text='Testpmd args:')
+				self.cb_as = flx.CheckBox(text="auto-start")
 				self.cb_crc = flx.CheckBox(text="disable-crc-strip")
 				self.cb_fia = flx.CheckBox(text="flow-isolate-all")
-			with flx.HBox():
-				ui.Label(text='--rxq:')
-				self.rxq = ui.LineEdit(text='1')
-				ui.Label(text='--rxd:')
-				self.rxd = ui.LineEdit(text='64')
-				ui.Label(text='--txq:')
-				self.txq = ui.LineEdit(text='1')
-				ui.Label(text='--txd:')
-				self.txd = ui.LineEdit(text='64')
-				ui.Label(text='--hairpin:')
-				self.hairpin = ui.LineEdit(text='1')
-
+			with flx.HBox(flex=1):
+				ui.Label(text='--rxq:', flex=0)
+				self.rxq = ui.LineEdit(text='1', flex=1)
+				ui.Label(flex=1,text='')
+				ui.Label(text='--rxd:', flex=0)
+				self.rxd = ui.LineEdit(text='64', flex=1)
+				ui.Label(flex=1,text='')
+				ui.Label(text='--txq:', flex=0)
+				self.txq = ui.LineEdit(text='1', flex=1)
+				ui.Label(flex=1,text='')
+				ui.Label(text='--txd:',flex=0)
+				self.txd = ui.LineEdit(text='64', flex=1)
+				ui.Label(flex=1,text='')
+				ui.Label(text='--hairpin:', flex=0)
+				self.hairpin = ui.LineEdit(text='0', flex=1)
+			with flx.HFix(flex=1, style=' text-align:left'):
+				self.cmdline = ui.Label(text='testpmd cmdline:', flex=10)
+				self.start = flx.Button(text='start', flex=1)
 
 	def get_testpmd_arg(self):
-		testpmd_arg = "--rxq="+self.rxq.text + " --txq=" + self.txq.text
+		testpmd_arg = "--rxq="+self.rxq.text + " --txq=" + self.txq.text + " --rxd=" + self.rxd.text + " --txd=" + self.txd.text
+		if self.hairpin.text != "0":
+			testpmd_arg += " --hairpin " + self.hairpin.text
+		if self.cb_crc.checked:
+			testpmd_arg += " --disable-crc-strip "
+		if self.cb_fia.checked:
+			testpmd_arg += " --flow-isolate-all "
+		if self.cb_as.checked:
+			testpmd_arg += " -a "
 		return testpmd_arg
+
+	@flx.reaction('start.pointer_click')
+	def add_widget(self, *events):
+		if len(self.root.dev_arg) == 0:
+			self.cmdline.set_text("Device not selected")
+			return
+		dev_list = ""
+		for pcid in self.root.dev_arg:
+			dev_list += " -w " + ''.join(pcid) + " "
+		testpmd_cmdline = "/root/test/dpdk.org/x86_64-native-linuxapp-gcc/app/testpmd " + self.root.eb.get_eal_arg() + dev_list + " -- " +self.get_testpmd_arg() + " -i"
+		self.cmdline.set_text(testpmd_cmdline)
+		
+
 
 class ItemBox(flx.PyWidget):
 
 	def init(self):
 		self.pattern = "pattern "
-		with flx.VBox():
-			with flx.HBox():
+		with flx.VBox(flex=1, style='border:2px solid gray;border-radius: 5px'):
+			with flx.HBox(flex=0):
 				ui.Label(text='pattern:')
 				self.item = ui.ComboBox(editable=True, selected_key='eth', options=('eth', 'ipv4', 'ipv6', 'tcp', 'udp', 'vlan', 'vxlan', 'tag'))
 				self.al = ui.Label(text='src:')
@@ -86,13 +124,38 @@ class ItemBox(flx.PyWidget):
 				self.showitem = ui.Label(text="No pattern")
 
 	def get_item(self):
-		return self.pattern + " end"
+		return self.pattern + " end "
 
 	@flx.reaction('add.pointer_click')
 	def add_widget(self, *events):
-		self.pattern = self.pattern + self.item.selected_key + "/"
-		ttext = self.pattern + "end"
-		self.showitem.set_text(ttext)
+		ttext = self.pattern + self.item.selected_key
+		if self.item.selected_index < 3:
+			if self.av.text != '0':
+				ttext += " src is " + self.av.text
+			if self.amv.text != '0':
+				ttext += " src mask " + self.amv.text
+			if self.bv.text != '0':
+				ttext += " dst is " + self.bv.text
+			if self.bmv.text != '0':
+				ttext += " dst mask " + self.bmv.text
+		elif self.item.selected_index < 5:
+			if self.av.text != '0':
+				ttext += " src is " + self.av.text
+			if self.bv.text != '0':
+				ttext += " dst is " + self.bv.text
+		elif self.item.selected_index is 5:
+			if self.av.text != '0':
+				ttext += " vid is " + self.av.text
+		elif self.item.selected_index is 6:
+			if self.av.text != '0':
+				ttext += " vni is " + self.av.text
+		elif self.item.selected_index is 7:
+			if self.av.text != '0':
+				ttext += " data is " + self.av.text
+
+		ttext += " / "
+		self.pattern = ttext
+		self.showitem.set_text(ttext + " end")
 
 	@flx.reaction('cl.pointer_click')
 	def cl_widget(self, *events):
@@ -103,25 +166,25 @@ class ItemBox(flx.PyWidget):
 	def combo_key_change(self):
 		if self.item.selected_index < 3:
 			self.al.set_text("src:")
-			self.av.set_text('1')
+			self.av.set_text('0')
 			self.am.set_text("src_mask:")
-			self.amv.set_text('1')
+			self.amv.set_text('0')
 			self.bl.set_text("dst:")
-			self.bv.set_text('1')
+			self.bv.set_text('0')
 			self.bm.set_text("dst_mask:")
-			self.bmv.set_text('1')
+			self.bmv.set_text('0')
 		elif self.item.selected_index < 5:
 			self.al.set_text("src_port:")
-			self.av.set_text('1')
+			self.av.set_text('-1')
 			self.am.set_text("dst_port:")
-			self.amv.set_text('1')
+			self.amv.set_text('-1')
 			self.bl.set_text("")
 			self.bv.set_text('')
 			self.bm.set_text("")
 			self.bmv.set_text('')
 		elif self.item.selected_index is 5:
 			self.al.set_text("vlan_id")
-			self.av.set_text('56')
+			self.av.set_text('0')
 			self.am.set_text("")
 			self.amv.set_text('')
 			self.bl.set_text("")
@@ -130,7 +193,7 @@ class ItemBox(flx.PyWidget):
 			self.bmv.set_text('')
 		elif self.item.selected_index is 6:
 			self.al.set_text("vxlan_id")
-			self.av.set_text('56')
+			self.av.set_text('0')
 			self.am.set_text("")
 			self.amv.set_text('')
 			self.bl.set_text("")
@@ -138,8 +201,8 @@ class ItemBox(flx.PyWidget):
 			self.bm.set_text("")
 			self.bmv.set_text('')
 		elif self.item.selected_index is 7:
-			self.al.set_text("tg_id")
-			self.av.set_text('56')
+			self.al.set_text("tag_id")
+			self.av.set_text('0')
 			self.am.set_text("")
 			self.amv.set_text('')
 			self.bl.set_text("")
@@ -147,28 +210,149 @@ class ItemBox(flx.PyWidget):
 			self.bm.set_text("")
 			self.bmv.set_text('')
 
+class ActionBox(flx.PyWidget):
+
+	def init(self):
+		with flx.VBox(style='border:2px solid gray;border-radius: 5px; align:left'):
+			with flx.VBox(style='border:2px solid gray;border-radius: 5px'):
+				with flx.HSplit(flex=1, style='text-align:left;'):
+					ui.Label(text='vxlan encap data:')
+					ui.Label(text='eth src:')
+					self.eth_src = ui.LineEdit(text='0')
+					ui.Label(text='eth dst:')
+					self.eth_dst = ui.LineEdit(text='0')
+					ui.Label(text='eth type:')
+					self.eth_type = ui.LineEdit(text='0x0800')
+					ui.Label(text='ip src:')
+					self.ip_src = ui.LineEdit(text='0')
+					ui.Label(text='ip dst:')
+					self.ip_dst = ui.LineEdit(text='0')
+					ui.Label(text='udp src:')
+					self.udp_src = ui.LineEdit(text='0')
+					ui.Label(text='udp dst:')
+					self.udp_dst = ui.LineEdit(text='4789')
+					ui.Label(text='vni:')
+					self.vni = ui.LineEdit(text='123')
+				with flx.HBox():
+					self.encap_disp = ui.Label(text='encap_data:')
+			with flx.HSplit():
+				self.smacs = flx.CheckBox(text="set_mac_src", flex=2)
+				self.smacs_v = ui.LineEdit(text='192.168.1.2', flex=2)
+				self.smacd = flx.CheckBox(text="set_mac_dst", flex=2)
+				self.smacd_v = ui.LineEdit(text='192.168.1.5', flex=2)
+				self.sip4s = flx.CheckBox(text="set_ipv4_src", flex=2)
+				self.sip4s_v = ui.LineEdit(text='192.168.1.2', flex=2)
+				self.sip4d = flx.CheckBox(text="set_ipv4_dst",flex=2)
+				self.sip4d_v = ui.LineEdit(text='192.168.1.5',flex=2)
+				self.stps = flx.CheckBox(text="set_tp_src",flex=2)
+				self.stps_v = ui.LineEdit(text='3456', flex=1)
+				self.stpd = flx.CheckBox(text="set_tp_dst", flex=2)
+				self.stpd_v = ui.LineEdit(text='3446', flex=1)
+			with flx.HBox():
+				self.cnt = flx.CheckBox(text="count")
+				self.decap = flx.CheckBox(text="vxlan_decap")
+				self.encap = flx.CheckBox(text="vxlan_encap")
+
+				self.stag = flx.CheckBox(text="set_tag")
+				self.stag_v = ui.LineEdit(text='12')
+
+				self.fate = ui.ComboBox(editable=True, selected_key='jump', options=('jump', 'drop', 'rss', 'queue', 'port id'))
+				self.fate_v = ui.LineEdit(text='2')
+				self.add = flx.Button(text='add')
+			with flx.HBox():
+				self.flow_detail = ui.Label(text="Flow to be added")
+
+	@flx.reaction('add.pointer_click')
+	def add_actions(self, *events):
+		print(self.root.fb.item.get_item())
+		if self.root.fb.item.get_item() == "pattern  end ":
+			self.flow_detail.set_text("Pattern not selected")
+			return
+
+		self.encap_v = "set vxlan ip-version ipv4 vni " + self.vni.text + " udp-src " + self.udp_src.text + " udp-dst " + self.udp_dst.text + " ip-src " + self.ip_src.text + " ip-dst " +\
+				self.ip_dst.text + " eth-src " + self.eth_src.text + " eth-dst " + self.eth_dst.text
+		self.encap_disp.set_text(self.encap_v)
+		self.action = " actions "
+		if (self.stag.checked):
+			self.action += " set_tag data " + self.stag_v.text + " / "
+		if (self.smacs.checked):
+			self.action += " set_mac_src mac_addr " + self.smacs_v.text + " / "
+		if (self.smacd.checked):
+			self.action += " set_mac_dst mac_addr " + self.smacd_v.text + " / "
+		if (self.sip4s.checked):
+			self.action += " set_ipv4_src ipv4_addr " + self.sip4s_v.text + " / "
+		if (self.sip4d.checked):
+			self.action += " set_ipv4_dst ipv4_addr " + self.sip4d_v.text + " / "
+		if (self.stpd.checked):
+			self.action += " set_tp_dst port " + self.stpd_v.text + " / "
+		if (self.stps.checked):
+			self.action += " set_tp_src port " + self.stps_v.text + " / "
+
+		if (self.cnt.checked):
+			self.action += " count / "
+		if (self.decap.checked):
+			self.action += " vxlan_decap / "
+		if (self.encap.checked):
+			self.action += " vxlan_encap / "
+
+		if (self.fate.selected_key == "jump"):
+			self.action += " jump group " + self.fate_v.text + " / "
+		elif (self.fate.selected_key == "port_id"):
+			self.action += " port_id id " + self.fate_v.text + " / "
+		elif (self.fate.selected_key == "queue"):
+			self.action += " queue index " + self.fate_v.text + " / "
+		elif (self.fate.selected_key == "rss"):
+			self.action += " rss / "
+		elif (self.fate.selected_key == "drop"):
+			self.action += " drop / "
+		self.action += " end"
+		flow_cmd = self.root.fb.get_attr_arg() + self.root.fb.item.get_item() + self.action
+		self.flow_detail.set_text(flow_cmd)
 
 
 class FlowBox(flx.PyWidget):
 
 	def init(self):
 		self.box=[]
-		with flx.VBox():
-			flx.Label(text='-------- Add flow --------', flex=1, css_class="center")
-			with flx.HBox():
+		with flx.VBox(flex=1, style='border:2px solid gray;border-radius: 5px'):
+			flx.Label(text='Add flow', flex=1, style='text-align:center')
+			with flx.HBox(style='border:2px solid gray;border-radius: 5px'):
 				ui.Label(text='attr:')
+				self.attr = ui.ComboBox(editable=True, selected_key='ingress', options=('ingress', 'egress', 'transfer'))
 				ui.Label(text='port_id:')
 				self.port_id = ui.LineEdit(text='0')
 				ui.Label(text='group:')
-				self.port_id = ui.LineEdit(text='1')
-				self.attr = ui.ComboBox(editable=True, selected_key='ingress', options=('ingress', 'egress', 'transfer'))
+				self.group_id = ui.LineEdit(text='1')
 			self.item = ItemBox()
+			self.action = ActionBox()
 
-	def get_testpmd_arg(self):
-		testpmd_arg = "--rxq="+self.rxq.text + " --txq=" + self.txq.text
-		return testpmd_arg
+	def get_attr_arg(self):
+		self.aarg = "flow create " + self.port_id.text + " group " + self.group_id.text + " " + self.attr.selected_key + " "
+		return self.aarg
+
+class ShowBox(flx.PyWidget):
+
+	def init(self):
+		self.box=[]
+		with flx.VBox(flex=1, style='border:2px solid gray;border-radius: 5px'):
+			flx.Label(text='Show', flex=1, style='text-align:center')
+
+
+class FocusBox(flx.VBox):
+	@flx.action
+	def focus(self):
+		self.node.focus()
 
 class TestpmdUI(flx.PyWidget):
+	CSS = """
+		.flx-MyWidget {
+			min-width: 10px;
+			min-height: 10px;
+			padding: 5px;
+			border: 2px solid black;
+			border-radius: 5px;
+	}
+	"""
 
 	def get_dev_info(self):
 		pci_l = subprocess.check_output("lspci -D | grep Mellanox | awk '{print $1}'", shell=True)
@@ -188,11 +372,13 @@ class TestpmdUI(flx.PyWidget):
 		self.mac_list=[]
 		self.dev_arg=[]
 		self.get_dev_info()
-		with flx.VBox(flex=1):
-			self.db=DeviceBox()
-			self.eb=EALBox()
-			self.ab=APPBox()
-			self.fb=FlowBox()
+		with flx.VBox(flex=1, style='border:2px solid gray;border-radius: 5px', tabindex=0):
+			with flx.VBox(flex=1):
+				self.db=DeviceBox(flex=0)
+				self.eb=EALBox(flex=0)
+				self.ab=APPBox(flex=0)
+				self.fb=FlowBox(flex=0)
+				self.show=ShowBox(flex=1)
 
               
 if __name__ == '__main__':
